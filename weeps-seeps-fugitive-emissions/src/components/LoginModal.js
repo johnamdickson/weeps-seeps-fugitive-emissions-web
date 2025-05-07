36c1d656e04d0f3db1077c1db2641f91a6c38aa5
johnamdickson <johnamdickson@me.com>
2025-05-07 03:19:31 +0000

    style: add toast for login success

--- weeps-seeps-fugitive-emissions/src/components/LoginModal.js
+++ weeps-seeps-fugitive-emissions/src/components/LoginModal.js
@@ -3,17 +3,27 @@ import { Modal, Button, Form } from 'react-bootstrap';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase'; // Make sure this is the correct import for your Firebase config
 import './LoginModal.css';
+import { useToast } from "../contexts/ToastContext";
 
 const LoginModal = ({ show, onHide }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const { showToast } = useToast();   // Toast message handler
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+        showToast(
+        "Welcome to the Weeps, Seeps and Fugitive Emissions Monitoring Tool.",
+        "success",
+        <>
+          <span className="material-icons success-icon me-3">thumb_up</span>
+          Login Successful
+        </>
+      );
       const user = userCredential.user;
       console.log("✅ Login successful:", user);
       // Optionally log additional details, such as the user's UID or email
@@ -22,7 +32,15 @@ const LoginModal = ({ show, onHide }) => {
 
       onHide();  // Close the modal on successful login
     } catch (err) {
-      setError(err.message);  // Store the error message for UI display
+      setError(err.message); 
+      showToast(
+        `There was a problem logging you in:  ${err.message}`,
+        "danger",
+        <>
+          <span className="material-icons success-icon me-3">thumb_down</span>
+          Login Error
+        </>
+      ) // Store the error message for UI display
       console.error("❌ Login failed:", err.code, err.message);  // Log detailed error information
     }
   };
@@ -33,9 +51,9 @@ const LoginModal = ({ show, onHide }) => {
         <Modal.Title>Login</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {error && <p className="text-danger">{error}</p>}
+        {error && <p className="text-white fs-6">{error}</p>}
         <Form onSubmit={handleLogin}>
-          <Form.Group className="mb-3">
+          <Form.Group className="mb-3 fs-5">
             <Form.Label>Email address</Form.Label>
             <Form.Control
               type="email"
@@ -46,7 +64,7 @@ const LoginModal = ({ show, onHide }) => {
             />
           </Form.Group>
 
-          <Form.Group className="mb-3">
+          <Form.Group className="mb-3 fs-5">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
@@ -57,7 +75,7 @@ const LoginModal = ({ show, onHide }) => {
           </Form.Group>
 
           <Button variant="primary" type="submit" className="btn-login">
-            <span class="material-symbols-outlined">input</span>
+            <span class="material-icons login-modal-icon">login</span>
             Login
             </Button>
         </Form>
