fa31c23309286c57a5bc1823129e72b7bf107623
johnamdickson <johnamdickson@me.com>
2025-05-08 03:51:10 +0000

    style: further styling home page content box

--- weeps-seeps-fugitive-emissions/src/pages/Home.js
+++ weeps-seeps-fugitive-emissions/src/pages/Home.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import './Home.css'; // Update your CSS file name accordingly
+import LoginModal from '../components/LoginModal';
 
 const HomePage = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <div className="home-page">
-        <Container fluid className="text-white d-inline-flex mt-5 me-lg-5 p-4 justify-content-end">
+        <Container fluid className="text-white d-inline-flex mt-5 p-4 justify-content-end">
           <Row className="justify-content-end">    
-            <Col className="me-lg-5" lg="9" sm="12">
+            <Col className="me-lg-5 pt-5 pe-4" lg="9" md="12">
             <Container fluid>
             <Row className="justify-content-end me-lg-5">    
-            <Col className="content-box me-lg-5 mx-auto" lg="6" sm="10">
-             <h1 className="display-5 fw-bold">Welcome to the<br />Emissions Monitoring Tool</h1>
-          <p className="lead">
+            <Col className="content-box me-lg-5 mx-auto" lg="7" sm="10">
+             <h1 className="display-5 fw-bold p-3">Welcome to the<br />Emissions Monitoring Tool</h1>
+          <p className="lead p-3">
             Your one stop shop to manage all aspects of fugitive emissions monitoring on your facility. 
             Select from one of the options below to continue.
           </p>
-          <div className="button-group mt-4">
-            <Button variant="danger" className="me-3">Join Now</Button>
-            <Button variant="light">Log In</Button>
+          <div className="button-group mt-1">
+            <Row className='justify-content-around'>
+              <Col lg="5" md="10">
+              <Button variant="danger" className="w-100 mt-2">Join Now</Button>
+              </Col >
+                            <Col lg="5" md="10">
+              <Button variant="light" className="w-100 mt-2" onClick={() => setShowLogin(true)}>Log In</Button>
+              </Col>
+            </Row>
+                       
           </div>
           </Col> 
           </Row>
@@ -27,6 +36,7 @@ const HomePage = () => {
           </Row>
 
         </Container>
+        <LoginModal show={showLogin} onHide={() => setShowLogin(false)} />
       </div>
   );
 };
